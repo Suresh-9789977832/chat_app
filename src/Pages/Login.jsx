9a859e91dcab7706974b7bc0eb7d5a6bb9541dc9
Suresh@9789977832 <sanddysuresh@gmail.com>
2024-05-05 05:41:48 +0530

    final_commit

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -39,7 +39,7 @@ function Login() {
 
   
   return (
-    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+    <div className='flex flex-col items-center justify-center w-96  max-mmedi:w-80 max-msmall:w-[290px] mx-auto'>
       <div className=' p-4 h-full w-full bg-white-600 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-40  border-gray-100'>
         <h1 className=' text-3xl  font-semibold text-center text-gray-300'>Login</h1>
         <form>
